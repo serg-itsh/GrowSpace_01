fb7adc0f932c4f9eb428273826a9760e28ef0b8b
Sergey Shevchenko <98112335+serg-itsh@users.noreply.github.com>
2025-04-12 12:58:30 +0300

    change import prisma

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,7 +1,9 @@
 
 // import { prisma } from "@/app/util/db";
-import { prisma } from "./util/db";
+// import { prisma } from "./util/db";
+
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { prisma } from "@/lib/prisma";
 // import { redirect } from "next/navigation";
 import Link from "next/link";
 import { buttonVariants } from "@/components/ui/button";
@@ -27,7 +29,8 @@ export default async function Dashboard() {
   const { getUser } = getKindeServerSession();
   const user = await getUser();
 
-  const data = await getData(user?.id);
+  // const data = await getData(user?.id);
+  const data = await getData(user.id);
 
   
 
